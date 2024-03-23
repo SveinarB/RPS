722341eb969d8554a92d7c02ac8a518d71e87ef4
Sveinar Bjørnseth <sveinar.bjornseth@gmail.com>
2024-03-23 18:40:55 +0100

    add a function that keeps score in a BO5 game

--- javascript.js
+++ javascript.js
@@ -16,8 +16,6 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 function playRound(playerSelection, computerSelection) {
     if(computerSelection === playerSelection) {
         return tie;
@@ -31,8 +29,39 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
 
+    for(let round = 1; round <= 5; round++) {
+        const playerSelection = prompt("Enter your choice (rock, paper or scissors): ").toLowerCase();
+        const computerSelection = getComputerChoice();
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+        console.log(`Round ${round}:`);
+        console.log(`Player chooses: ${playerSelection}`);
+        console.log(`Computer chooses: ${computerSelection}`);
+
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+
+        if (result === playerWin) {
+            playerScore++
+        } else if (result === computerWin) {
+            computerScore++
+        }
+    }
+
+    console.log("\nGame over!\n");
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You win the game!");
+    } else if (computerScore > playerScore) {
+        console.log("Sorry! Computer wins the game!");
+    } else {
+        console.log("It's a tie game!");
+    }
+}
+
+playGame();
